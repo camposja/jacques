9b94de421846b19904d1b2edea6518cd57934a4f
Jose A. Campos <jjoseccampos@gmail.com>
2017-04-25 15:29:35 -0400

    deleted test helper extra code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,3 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
-DatabaseCleaner.strategy = :truncation
-
-class ActiveSupport::TestCase
-  self.use_transactional_tests = false
-
-  def setup
-    DatabaseCleaner.start
-  end
-
-  def teardown
-    DatabaseCleaner.clean
-  end
-
-end
